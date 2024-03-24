1dd25d642fe57c58bf2bedda9390234c2936e4d7
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-03-24 21:28:50 +0530

    fixed typo

--- controllers/Flag.js
+++ controllers/Flag.js
@@ -3,7 +3,7 @@ const submitFlag = async (req, res) => {
   try {
     const { flag } = req.body;
     const { id } = req.user;
-    const user = await User.findBtId(id);
+    const user = await User.findById(id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
