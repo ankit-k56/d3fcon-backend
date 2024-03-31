3fa7b150d2adea0910c59fdba6ba3093850f2ca4
Ankit <ankitkumar19041@gmail.com>
2024-03-31 06:36:46 +0530

    collection update

--- models/User.js
+++ models/User.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  roll: { type: String, unique: true },
-  tryhackmeId: { type: String },
-  password: { type: String },
-  year: { type: String, required: true },
-  rate: { type: String, required: true },
-  isEventAttended: { type: Boolean, default: false },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    roll: { type: String, unique: true },
+    tryhackmeId: { type: String },
+    password: { type: String },
+    year: { type: String, required: true },
+    rate: { type: String, required: true },
+    isEventAttended: { type: Boolean, default: false },
+  },
+  { collection: "users" }
+);
 
-module.exports = mongoose.model("User", userSchema, "User");
+module.exports = mongoose.model("User", userSchema);
